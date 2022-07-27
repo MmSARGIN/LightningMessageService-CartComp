8917c701dea2753be40ceaad48b94c71536b69d7
MmSARGIN <melih.sargin.53@gmail.com>
2022-07-27 19:03:27 +0300

    paymentForm

--- main/default/lwc/addedCart/addedCart.js
+++ main/default/lwc/addedCart/addedCart.js
@@ -2,6 +2,7 @@ import { LightningElement, wire, track } from 'lwc';
 import { subscribe, MessageContext } from 'lightning/messageService';
 import ADD_TO_CART from '@salesforce/messageChannel/Add_Cart__c';
 import { ShowToastEvent } from 'lightning/platformShowToastEvent';
+import addNewOrder from "@salesforce/apex/AddOrder.addNewOrder";
 
 export default class AddedCart extends LightningElement {
     
@@ -18,12 +19,18 @@ export default class AddedCart extends LightningElement {
     itemid;
     countProducts = 0;
     remainingNumber = 0;
-    
-    
+    confirmForm = false;
+    formName ='';
+    formLastName = '';
+    formPhone;
+    formCard;
+    formCVV;
+    formObj;
+    formDate;
+    formAddress;
     
     @wire(MessageContext)
     messageContext;
-
     connectedCallback(){
         this.subscribeToMessageChannel();
     }
@@ -42,7 +49,7 @@ export default class AddedCart extends LightningElement {
             this.countProducts += 1;
             this.added.push(this.products[message.targetname.split("-")[0] - 1]);
             this.price += this.products[parseInt(this.itemid - 1)].price;
-            this.comingPrice = this.price.toFixed(2);
+            this.comingPrice = Math.round(this.price);
             const evt = new ShowToastEvent({
                     title: 'Success Add',
                     message: 'Your product has been added to the cart.',
@@ -59,7 +66,7 @@ export default class AddedCart extends LightningElement {
                      }
                      if(message.targetname.split("-")[0] == this.added[this.indexOfProduct].id){
                         this.price = this.price - this.products[parseInt(this.itemid - 1)].price;
-                        this.comingPrice = this.price.toFixed(2);
+                        this.comingPrice = Math.round(this.price);
                         this.added.splice(this.indexOfProduct, 1);
                         this.countProducts -= 1;
                         const evt = new ShowToastEvent({
@@ -74,4 +81,103 @@ export default class AddedCart extends LightningElement {
             }
     console.log("added :" ,this.added);
 }
+
+handleClick(){
+    console.log('tiklandi');
+    if(this.countProducts !== 0){
+        this.confirmForm = !this.confirmForm;
+    }else{
+        const evt = new ShowToastEvent({
+            title: 'Sepete Urun ekleyin',
+            message: 'Bos gonderemezsiniz',
+            variant: 'error',
+            mode: 'dismissable'
+        });
+        this.dispatchEvent(evt);
+    }
+   
+}
+
+handleChange(event){
+    console.log(event.target.value);
+    
+    if(event.target.name === 'name'){
+        this.formName = event.target.value;
+    }else if(event.target.name === 'lastname'){
+        this.formLastName = event.target.value;
+    } else if(event.target.name === 'phone'){
+        this.formPhone = event.target.value;
+    }else if(event.target.name === 'card'){
+        this.formCard = event.target.value;
+    }else if(event.target.name === 'cvv'){
+        this.formCVV = event.target.value;
+    }else if(event.target.name === 'date'){
+        this.formDate = event.target.value;
+    }else if(event.target.name === 'address'){
+        this.formAddress = event.target.value;
+    }
+
+     
+}
+handleConfirm(){
+    this.formObj ={
+        Name : this.formName,
+        LastName :this.formLastName,
+        Phone : this.formPhone,
+        CardNumber : this.formCard,
+        CVV: this.formCVV,
+        Date:this.formDate,
+        Address:this.formAddress,     
+        Date_Time:Date(),  
+        Orders:this.added,
+        Price:this.comingPrice,
+        OrderId: Math.random()
+    }
+    if(this.formName !== '' && this.formLastName !== '' && this.formCard !== '' && this.formCVV !== ''){
+        console.log(this.formObj)
+        addNewOrder({
+            name:this.formName,
+            lastname: this.formLastName,
+            phone:this.formPhone,
+            address: this.formAddress,
+            cvv:this.formCVV,
+            card:this.formCard,
+            orderdate:this.formObj.Date_Time,
+            orderid: this.formObj.OrderId,
+            price:this.formObj.Price
+
+        })
+        .then(res => console.log(res))
+        this.confirmForm = !this.confirmForm;
+        this.added = [];
+        this.countProducts = 0;
+        this.comingPrice = 0;
+        this.price = 0;
+        const evt = new ShowToastEvent({
+            title: 'Success Payment',
+            message:'Your orders has been shipped.',
+            variant: 'success',
+            mode: 'dismissable'
+        });
+        this.dispatchEvent(evt);
+    }else {
+        console.log('olmadi kanka')
+        const evt = new ShowToastEvent({
+            title: 'Error',
+            message:'Fill the required fields.',
+            variant: 'error',
+            mode: 'dismissable'
+        });
+        this.dispatchEvent(evt);
+    }
+    this.formName = '';
+    this.formLastName = '';
+    this.formCard = '';
+    this.formCVV = '';
+    this.formDate = '';
+    this.formPhone = '';
+    
+}
+
+
 }
